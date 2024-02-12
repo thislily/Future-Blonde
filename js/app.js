17666e48a7baf2cd311961dbb26b98d2aed05d6f
thislily <lilyclairewatson@gmail.com>
2024-02-12 19:00:34 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -12,7 +12,6 @@ document.addEventListener("DOMContentLoaded", () => {
 if (
   location.pathname === "/html/about.html" ||
   location.pathname === "/html/about" ||
-  location.pathname === "/index.html" ||
   location.pathname === "/index" ||
   location.pathname === "/html/contact.html" ||
   location.pathname === "/html/contact" ||
