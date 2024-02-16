f0c7dbb0dd74c4c7797e037b7f4e6bbdfb34edfe
thislily <lilyclairewatson@gmail.com>
2024-02-16 14:46:26 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,6 @@ import { blogPage, getPosts, seeMorePosts } from "./render/fetch-posts.js";
 import { postPage } from "./render/create-post.js";
 import { createLightbox } from "./UI/lightbox.js";
 import { handleContactForm } from "./forms/contact.js";
-import { handleSubscribeForm } from "./forms/subscribe.js";
 if (location.pathname === "/" || location.pathname === "/index.html") {
   handleCarousel();
 }
@@ -15,7 +14,6 @@ document.addEventListener("DOMContentLoaded", () => {
   handleNav();
   getPosts();
   blogPage();
-  // handleSubscribeForm();
 });
 
 if (scroller) {
